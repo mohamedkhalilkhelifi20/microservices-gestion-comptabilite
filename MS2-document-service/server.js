635948eecfb53585dcd90ce4770b6c7f51c7b86e
Mohamed Khalil Khelifi <mohamedkhalil.khelifi@polytechnicien.tn>
2026-05-12 19:48:40 +0100

    add server.js in MS2

---
+++ MS2-document-service/server.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const path        = require('path');
+const grpc        = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const initDatabase             = require('./db/database');
+const { disconnect: kafkaOff } = require('./kafka/producer');
+const handlers                 = require('./handlers/documentHandlers');
+
+const PORT      = process.env.MS2_PORT || '50052';
+const PROTO_DIR = path.join(__dirname, 'proto');
+
+const LOADER_OPTIONS = {
+    keepCase:    true,
+    longs:       String,
+    enums:       String,
+    defaults:    true,
+    oneofs:      true,
+    includeDirs: [PROTO_DIR],
+};
+
+const invoicePkg = grpc.loadPackageDefinition(
+    protoLoader.loadSync(path.join(PROTO_DIR, 'invoice.proto'), LOADER_OPTIONS)
+).invoice;
+
+const declarationPkg = grpc.loadPackageDefinition(
+    protoLoader.loadSync(path.join(PROTO_DIR, 'declaration.proto'), LOADER_OPTIONS)
+).declaration;
+
+const alertePkg = grpc.loadPackageDefinition(
+    protoLoader.loadSync(path.join(PROTO_DIR, 'alerte.proto'), LOADER_OPTIONS)
+).alerte;
+
+function buildServer() {
+    const server = new grpc.Server();
+
+    server.addService(invoicePkg.InvoiceService.service, {
+        createInvoice:     handlers.createInvoice,
+        signInvoice:       handlers.signInvoice,
+        getInvoice:        handlers.getInvoice,
+        getClientInvoices: handlers.getClientInvoices,
+    });
+
+    server.addService(declarationPkg.DeclarationService.service, {
+        createDeclaration:   handlers.createDeclaration,
+        validateDeclaration: handlers.validateDeclaration,
+        getDeclaration:      handlers.getDeclaration,
+    });
+
+    server.addService(alertePkg.AlerteService.service, {
+        createAlerte: handlers.createAlerte,
+        getAlertes:   handlers.getAlertes,
+    });
+
+    return server;
+}
+
+async function start() {
+    await initDatabase();
+
+    const server = buildServer();
+
+    server.bindAsync(
+        `0.0.0.0:${PORT}`,
+        grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+            if (err) {
+                console.error('[MS2] Erreur démarrage gRPC :', err.message);
+                process.exit(1);
+            }
+            console.log(`[MS2] gRPC server démarré sur le port ${port}`);
+        }
+    );
+
+    process.on('SIGINT',  () => shutdown(server));
+    process.on('SIGTERM', () => shutdown(server));
+}
+
+async function shutdown(server) {
+    console.log('\n[MS2] Arrêt en cours...');
+    await kafkaOff();
+    server.tryShutdown(() => {
+        console.log('[MS2] Serveur gRPC arrêté');
+        process.exit(0);
+    });
+}
+
+start();
